0d416a34811f7caab6157d3fa8d00970cf4088ba
Digontha <digonthodj@gmail.com>
2023-12-06 19:49:26 +0600

    create best doctors collection and get this data

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+require('dotenv').config()
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jp082z4.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    const bestCollection = await client.db("DoctorDB").collection("bestDoctors");
+
+    app.get("/bestDoctors",async(req,res)=>{
+      const result = await bestCollection.find().toArray();
+      res.send(result);
+    })
+   
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
